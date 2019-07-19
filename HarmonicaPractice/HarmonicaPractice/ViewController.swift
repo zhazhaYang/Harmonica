e8a96b9e1d5a0f493234d4190464ebc92741cea0
zhazhaYang <2357031598@qq.com>
2019-07-18 23:52:53 -0700

    First

---
+++ HarmonicaPractice/HarmonicaPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HarmonicaPractice
+//
+//  Created by yang on 2019/7/18.
+//  Copyright © 2019 yang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
