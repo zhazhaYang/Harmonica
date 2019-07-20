3d6a18d15fe3304cfc25b82b55dab693e81ef580
zhazhaYang <2357031598@qq.com>
2019-07-19 23:01:34 -0700

    MmenusAndSwitchView

--- HarmonicaPractice/HarmonicaPractice/ViewController.swift
+++ HarmonicaPractice/HarmonicaPractice/ViewController.swift
@@ -8,12 +8,30 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate, NSTabViewDelegate {
+    
+    @IBOutlet weak var firstTableView: NSTableView!
+    @IBOutlet weak var secondTableView: NSTableView!
+    @IBOutlet weak var tabView: NSTabView!
+    
+    private let firstTableID = "FirstTable"
+    private let secondTableID = "SecondTable"
+    private let firstCellID = "IDFirstCell"
+    private let secondCellID = "IDSecondCell"
 
+    private let firstStr = ["口琴练习"]
+    private let firstImage = ["harmonica"]
+    private let secondStr = ["本地录音"]
+    private let secondImage = ["audio"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        firstTableView.delegate = self
+        firstTableView.dataSource = self
+        secondTableView.delegate = self
+        secondTableView.dataSource = self
     }
 
     override var representedObject: Any? {
@@ -22,6 +40,43 @@ class ViewController: NSViewController {
         }
     }
 
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if tableView.identifier!.rawValue == firstTableID {
+            return firstStr.count
+        }
+        return secondStr.count
+    }
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+                var cellID: String? = nil
+                var str: String? = nil
+                var image = NSImage()
+                if tableView.identifier!.rawValue == firstTableID {
+                    cellID = firstCellID
+                    str = firstStr[row]
+                    image = NSImage.init(named: firstImage[row])!
+                    
+                } else if tableView.identifier!.rawValue == secondTableID {
+                    cellID = secondCellID
+                    str = secondStr[row]
+                    image = NSImage.init(named: secondImage[row])!
+                }
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellID!), owner: nil) as? NSTableCellView {
+                    cell.textField?.stringValue = str!
+                    cell.imageView?.image = image
+                    return cell
+                }
+                return nil
+    }
+
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        let firSelectedRow = firstTableView.selectedRow
+        let secSelectedRow = secondTableView.selectedRow
+        if firSelectedRow >= 0 {
+            tabView.selectTabViewItem(at: firSelectedRow)
+        } else {
+            tabView.selectTabViewItem(at: firstStr.count + secSelectedRow)
+        }
+    }
 
 }
 
